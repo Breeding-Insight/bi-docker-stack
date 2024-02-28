008a4f00fc02fdedcb2981a3ab38c2004c092e9e
HMS17 <84345306+HMS17@users.noreply.github.com>
2024-02-28 17:47:05 -0500

    Clean startbiapi.sh docker commands

--- .devcontainer/startbiapi.sh
+++ .devcontainer/startbiapi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Starts biapi
 git submodule update --init --recursive
-docker-compose -f docker-compose.yml -f docker-compose-redis.yml -f docker-compose-localstack.yml -f docker-compose-gigwa.yml -f docker-compose-dev.yml build biapi
-docker-compose -f docker-compose.yml -f docker-compose-redis.yml -f docker-compose-localstack.yml -f docker-compose-gigwa.yml -f docker-compose-dev.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose-redis.yml -f docker-compose-localstack.yml -f docker-compose-gigwa.yml -f docker-compose-dev.yml up -d --build biapi brapi-server mailhog bidb biproxy redis localstack gigwa mongo
 wait
